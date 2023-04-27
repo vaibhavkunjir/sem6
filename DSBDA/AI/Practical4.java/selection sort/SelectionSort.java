2e4326acc1e74b98dc388bcc8309849d8e88acd9
vaibhavkunjir <130646241+vaibhavkunjir@users.noreply.github.com>
2023-04-27 12:58:52 +0530

    Add files via upload

---
+++ DSBDA/AI/Practical4.java/selection sort/SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+        System.out.println("Before sorting: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("After sorting: " + Arrays.toString(arr));
+    }
+}
